405942777ed4a715cb3baa4f5ddbb140f0d2a9aa
Dmitry <dean.grimm@yandex.ru>
2023-01-07 22:04:05 +0800

    задача 15.1

--- homework/task 15/Program.cs
+++ homework/task 15/Program.cs
@@ -2,5 +2,9 @@
 // день недели, и проверяет, является ли этот день выходным.
 Console.WriteLine ("Введите число от 1 до 7 обозначающую день недели");
 int x = Convert.ToInt32 (Console.ReadLine());
-if (x == 6 && x == 7) Console.WriteLine ("выходной");
+if (x == 6 || x == 7) 
+{
+    Console.WriteLine ("выходной");
+}
+else if (x > 7) Console.WriteLine ("Некорректное число");
 else Console.WriteLine ("Будни");
