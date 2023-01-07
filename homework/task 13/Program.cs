48d73ed297d4fddfe880be95a8e4ad1e821e22e0
Dmitry <dean.grimm@yandex.ru>
2023-01-07 21:42:37 +0800

    задача 13

--- homework/task 13/Program.cs
+++ homework/task 13/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int x1 = x;
+int i = 0;
+while (x > 0)
+{
+    x = x / 10;
+    i++;
+}
+Console.WriteLine("колличество цифр в числе " + i);
+if (i < 3)
+{
+    Console.Write("третьей цифры в числе нет");
+}
+else  
+{
+    int count = 3;
+    while (i > count)
+    {
+        x1 = x1 / 10;
+        count++;
+    }
+    x1 = x1 % 10;
+    Console.Write("третья цифра в числе " + x1);
+}
